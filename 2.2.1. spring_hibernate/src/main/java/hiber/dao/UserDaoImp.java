4b9b9c21d93a23b6c947ac3ae51fca47f3ea40b2
Renat_Ahmedyanov <kaowood@gmail.com>
2022-10-11 23:14:07 +0300

    Java-pre-project-2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,6 +1,7 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -22,8 +23,22 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("FROM User");
       return query.getResultList();
    }
 
+   @Override
+   public User getUserByCar(String model, int series) {
+      User user = null;
+      try {
+         TypedQuery<User> query = sessionFactory.getCurrentSession()
+                 .createQuery("FROM User u WHERE u.car.model = :model AND u.car.series = :series")
+                 .setParameter("model", model)
+                 .setParameter("series", series);
+         user = query.getSingleResult();
+      } catch (Exception e) {
+         System.out.println("Can't find user");
+      }
+      return user;
+   }
 }
